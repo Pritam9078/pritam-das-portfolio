0231c1a28bc575822c51a7b0a5f3ed5ee90c0b7e
Pritam Das <dpritam2708@gmail.com>
2026-05-07 20:28:15 +0530

    Fix TypeScript errors causing Vercel build failure

--- src/components/MainContainer.tsx
+++ src/components/MainContainer.tsx
@@ -2,7 +2,6 @@ import { lazy, PropsWithChildren, Suspense, useEffect, useState } from "react";
 import About from "./About";
 import Career from "./Career";
 import Contact from "./Contact";
-import Cursor from "./Cursor";
 import Landing from "./Landing";
 import Navbar from "./Navbar";
 import SocialIcons from "./SocialIcons";
